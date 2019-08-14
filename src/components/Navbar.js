2486417d9c472fc50456394a8505ae76f4d28cd4
Puspitavabna <41649263+Puspitavabna@users.noreply.github.com>
2019-08-14 23:20:22 +0600

    Create different directory and file

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+export default Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello From Navbar</h3>
+            </div>
+        );
+    }
+}
+
---
+++ src/components/ProductList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Product from "./Product";
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+             <h3>Hello from Productlist</h3>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
